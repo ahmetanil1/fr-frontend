151e996db0b7f3b74e96680943b10fbcf20f4845
ahmetanil1 <amtyrgnc@gmail.com>
2024-12-02 16:50:49 +0300

    add close screen when it click another place

--- components/header/hamburgerMenu.js
+++ components/header/hamburgerMenu.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoReturnDownBackSharp } from "react-icons/io5";
 import Input from "../general/Input";
@@ -7,6 +7,7 @@ import HamburgerMenuContent from "../HamburgerMenuContent";
 
 function HamburgerMenu() {
     const [openMenu, setOpenMenu] = useState(false);
+    const menuRef = useRef(null); // Ref to the menu container
 
     const toggleMenu = () => {
         setOpenMenu(!openMenu);
@@ -20,6 +21,23 @@ function HamburgerMenu() {
         setOpenMenu(false);
     };
 
+    // Close the menu when clicking outside the menu
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+                setOpenMenu(false); // Close the menu
+            }
+        };
+
+        // Add event listener on mount
+        document.addEventListener("mousedown", handleClickOutside);
+
+        // Clean up event listener on unmount
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
     return (
         <div className="relative">
             <div className="flex z-50">
@@ -31,15 +49,14 @@ function HamburgerMenu() {
             </div>
 
             <div
-                className={`fixed left-0 mt-9 mx-64 h-3/5 w-auto bg-white dark:bg-black z-50 transform ${openMenu ? "scale-100" : "scale-0"
+                ref={menuRef} // Attach the ref here
+                className={`rounded-md fixed left-0 mt-9 mx-64 h-3/5 w-8/12  bg-white dark:bg-black z-50 transform ${openMenu ? "scale-100" : "scale-0"
                     } ${openMenu ? "ease-out transition duration-500" : ""} shadow-lg flex`}
             >
-               
-
                 {/* Content - Ortada */}
                 <div
                     className="flex flex-col p-4 space-y-4 w-full text-black dark:text-white"
-                    onClick={(e) => e.stopPropagation()}
+                    onClick={preventMenuClose}
                 >
                     <div
                         className="absolute top-4 right-4 cursor-pointer"
@@ -52,8 +69,8 @@ function HamburgerMenu() {
                         <Input />
                     </div>
 
-                    <div className="flex flex-col space-y-4 text-center">
-                        <HamburgerMenuContent />
+                    <div className="flex flex-col space-y-4 text-center  max-h-[80vh]">
+                        <HamburgerMenuContent onCategoryClick={closeMenuAndGoHome} />
                     </div>
                 </div>
             </div>
