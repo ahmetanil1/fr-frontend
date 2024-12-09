c092363fd02821fad6cffce55c68781680673cd5
ahmetanil1 <amtyrgnc@gmail.com>
2024-12-09 15:15:48 +0300

    Fetched single user

--- services/users.js
+++ services/users.js
@@ -1,7 +1,7 @@
 import config from "@/config.json"
 export async function getUsers() {
     try {
-        const response = await fetch(config.backend_url + "/users", {
+        const response = await fetch(config.backend_url + "/user", {
             method: "GET",
             credentials: "include",
         });
@@ -17,4 +17,26 @@ export async function getUsers() {
         console.log("hata:", error);
         return [];
     }
+}
+
+
+export async function fetchSingleUser(id) {
+    try {
+        const response = await fetch(config.backend_url + `/user/${id}`, {
+            method: "GET",
+            credentials: "include",
+        });
+        if (response.ok) {
+            const data = await response.json();
+            return data;
+        } else {
+            console.log("Users couldn't be fetched");
+            return null;
+        }
+    } catch (error) {
+
+        console.log("hata:", error);
+        return null
+    }
+
 }
