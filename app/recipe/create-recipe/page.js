df28e9da692f9be45968a9c1cd6167ad6f14cc58
ahmetanil1 <amtyrgnc@gmail.com>
2024-12-15 17:27:55 +0300

    Add some new sections

--- app/recipe/create-recipe/page.js
+++ app/recipe/create-recipe/page.js
@@ -2,10 +2,11 @@
 "use client";
 import React, { useState } from 'react';
 import { toast } from 'react-toastify';
-import { FaCamera } from "react-icons/fa6";
+import { FaC, FaCamera } from "react-icons/fa6";
 import { FaTrash } from "react-icons/fa";
 import config from "@/config.json";
 import { useRouter } from 'next/navigation';
+import { images } from '@/next.config';
 
 function CreateRecipe() {
     const [steps, setSteps] = useState([""]); // Initially one empty step
@@ -20,6 +21,11 @@ function CreateRecipe() {
     const [portion, setPortion] = useState("");
     const [cookingTime, setCookingTime] = useState("");
     const [imageName, setImageName] = useState('');
+    const [calories, setCalories] = useState("");
+    const [fats, setFats] = useState("");
+    const [proteins, setProteins] = useState("");
+    const [carbs, setCarbs] = useState("");
+    const [imagesGallery, setImagesGallery] = useState(['']);
     const router = useRouter();
     // Pişirme ve püf noktaları açıklama ve fotoğraf ekleme
 
@@ -59,6 +65,19 @@ function CreateRecipe() {
     const addNewStep = () => {
         setSteps([...steps, ""]);
     };
+
+    const handleCalories = (e) => {
+        setCalories(e.target.value);
+    }
+    const handleFats = (e) => {
+        setFats(e.target.value);
+    }
+    const handleProteins = (e) => {
+        setProteins(e.target.value);
+    }
+    const handleCarbs = (e) => {
+        setCarbs(e.target.value);
+    }
 
     const handleStepChange = (index, value) => {
         const updatedSteps = [...steps];
@@ -124,6 +143,13 @@ function CreateRecipe() {
         updatedCookingTips[index].photoUrl = URL.createObjectURL(e.target.files[0]);
         setCookingTips(updatedCookingTips);
     };
+
+    const handleAddImagesGallery = () => {
+        setImagesGallery([...imagesGallery, '']);
+    }
+    const handleRemoveImagesGallery = (index) => {
+        setImagesGallery(imagesGallery.filter((_, i) => i !== index));
+    }
 
     const handleServingTime = (e) => {
         setServingTime(e.target.value);
@@ -207,6 +233,69 @@ function CreateRecipe() {
                             onChange={(e) => handleAddImage(e, 0)}
                         />
                     </div>
+
+                    <div className='flex flex-col gap-4'>
+                        <h2 className='text-lg'>Besin değerleri</h2>
+                        <div className='flex justify-between gap-4 px-4 py-10  border border-gray-400 hover:border-gray-600 rounded-md border-dashed'>
+                            <div>
+                                <h3 className='flex items-center justify-center mb-2'>Kalori</h3>
+                                <input
+                                    type='number'
+                                    className='w-32 h-8 px-2'
+                                    placeholder='0 (cal)'
+                                    value={calories}
+                                    onChange={handleCalories}
+                                    name='calories'
+                                    min="1"
+                                    id='calories'
+                                    typeof="number"
+                                />
+                            </div>
+                            <div>
+                                <h3 className='flex items-center justify-center mb-2'>Yağ</h3>
+                                <input
+                                    type='number'
+                                    className='w-32 h-8 px-2'
+                                    placeholder='0 (g)'
+                                    value={fats}
+                                    onChange={handleFats}
+                                    name='fats'
+                                    min="1"
+                                    id='fats'
+                                    typeof="number"
+                                />
+                            </div>
+                            <div>
+                                <h3 className='flex items-center justify-center mb-2'>Protein</h3>
+                                <input
+                                    type='number'
+                                    className='w-32 h-8 px-2'
+                                    placeholder='0 (g)'
+                                    value={proteins}
+                                    onChange={handleProteins}
+                                    name='proteins'
+                                    min="1"
+                                    id='proteins'
+                                    typeof="number"
+                                />
+                            </div>
+                            <div>
+                                <h3 className='flex items-center justify-center mb-2'>Karbonhidrat</h3>
+                                <input
+                                    type='number'
+                                    className='w-32 h-8 px-2'
+                                    placeholder='0 (g)'
+                                    value={carbs}
+                                    onChange={handleCarbs}
+                                    name='carbs'
+                                    min="1"
+                                    id='carbs'
+                                    typeof="number"
+                                />
+                            </div>
+                        </div>
+                    </div>
+
 
                     {/* Form Inputs Section */}
                     <div className="flex flex-col sm:flex-row gap-4 border border-gray-300 hover:border-dashed hover:border-gray-600 py-10 sm:py-5 md:py-10 rounded-md">
@@ -323,7 +412,7 @@ function CreateRecipe() {
 
                     {/* Ingredients Section */}
                     <div>
-                        <div>Malzemeler</div>
+                        <div className='text-lg'>Malzemeler</div>
                         {ingredients.map((ingredient, index) => (
                             <div key={index} className="flex gap-4 my-3 flex-col sm:flex-row">
                                 <input
@@ -375,7 +464,7 @@ function CreateRecipe() {
 
                     {/* Cooking Tips Section */}
                     <div className="py-3">
-                        <h3>Nasıl Yapılır?</h3>
+                        <h3 className='text-lg'>Nasıl Yapılır?</h3>
                         {cookingTips.map((tip, index) => (
                             <div key={index} className="flex flex-col gap-4 my-3">
                                 <div className="flex justify-start items-center gap-4">
@@ -448,13 +537,68 @@ function CreateRecipe() {
 
                     {/* Video URL Section */}
                     <div>
-                        <h3 className="py-4">Video URL</h3>
+                        <h3 className="py-4 text-lg">Video URL</h3>
                         <input
                             className="py-2 px-2 w-full"
                             placeholder="Tarifinizin video linkini buraya ekleyebilirsiniz."
                             onChange={handleAddVideo}
                         />
                     </div>
+
+                    <div className="flex flex-col gap-4">
+                        {/* Galeri Başlığı */}
+                        <h3 className="text-lg font-bold flex justify-start text-center">Galeri</h3>
+
+                        {/* Resim Kutuları */}
+                        <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+                            {imagesGallery.map((image, index) => (
+                                <div key={index} className="relative group">
+                                    {image.imageUrl > 1 ? (
+                                        <div className="relative">
+                                            <input
+                                                id={`photo-upload-${index}`}
+                                                type="file"
+                                                accept="image/*"
+                                                className="sr-only"
+                                                onChange={(e) => handleAddImagesGallery(e, index)}
+                                            />
+                                            {image.photoUrl && (
+                                                <img
+                                                    src={image.photoUrl}
+                                                    alt={`Cooking Tip ${index}`}
+                                                    className="absolute inset-0 w-full h-full object-cover"
+                                                />
+                                            )}
+                                        </div>
+                                    ) : (
+                                        <div className="w-32 h-32 flex items-center justify-center border-2 border-dashed rounded-md cursor-pointer">
+                                            <label
+                                                htmlFor={`photo-upload-${index}`}
+                                                className="flex items-center justify-center cursor-pointer w-full h-full"
+                                            >
+                                                <div className="bg-gray-800 w-10 h-10 rounded-md  flex items-center justify-center">
+                                                    <FaCamera className="text-white" size={16} />
+                                                </div>
+                                            </label>
+                                        </div>
+                                    )}
+                                </div>
+                            ))}
+                        </div>
+
+                        {/* Fotoğraf Ekle Butonu */}
+                        <div className="text-center flex justify-start">
+                            <button
+                                type="button"
+                                onClick={handleAddImagesGallery}
+                                className="mt-4 text-white bg-gray-700 hover:bg-gray-800 px-6 py-2 rounded-md"
+                            >
+                                Fotoğraf Ekle
+                            </button>
+                        </div>
+                    </div>
+
+
 
                     {/* Submit Button */}
                     <div className="flex justify-center gap-4">
