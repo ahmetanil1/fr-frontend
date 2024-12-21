29858111b805c503ea845897ae4c07bafd0373a5
ahmetanil1 <amtyrgnc@gmail.com>
2024-12-21 05:55:08 +0300

    Add hr general component

---
+++ components/general/hr.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hr() {
+    return (
+        <div className='w-full'>
+            <hr className='border-gray-800 dark:border-gray-300 ' />
+        </div>
+    )
+}
+
+export default Hr
