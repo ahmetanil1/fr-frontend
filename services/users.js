e99be931518523a8dd44c96965a9bbab36eb4110
ahmetanil1 <amtyrgnc@gmail.com>
2025-01-22 19:24:42 +0300

    Add user services

--- services/users.js
+++ services/users.js
@@ -1,44 +1,196 @@
-import config from "@/config.json";
+import dotenv from "dotenv";
 
-export async function fetchAllUser() {
+const backend_url = process.env.BACKEND_URL;
+
+export async function GetSingleUser(user_id) {
     try {
-        const response = await fetch(config.backend_url + "/users", {
+        const response = await fetch(`${backend_url}/users/${user_id}`, {
             method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            credentials: "include"
+        });
+        if (response.ok) {
+            const data = await response.json();
+            console.log("User:", data);
+            return data;
+        } else {
+            throw new Error(response.statusText || "Unknown error");
+
+        }
+
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+export async function getSingleUserFollowers(user_id) {
+    try {
+        const response = await fetch(`${backend_url}/users/${user_id}/followers`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
             credentials: "include",
         });
         if (response.ok) {
             const data = await response.json();
+            console.log("User Followers:", data);
             return data;
         } else {
-            console.log("Users couldn't be fetched");
-            return [];
+            throw new Error(response.statusText || "Unknown error");
+
+        }
+
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+export async function getSingleUserFollowing(user_id) {
+    try {
+        const response = await fetch(`${backend_url}/users/${user_id}/followings`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            credentials: "include",
+        });
+        if (response.ok) {
+            const data = await response.json();
+            console.log("User Following:", data);
+            return data;
+        } else {
+            throw new Error(response.statusText || "Unknown error");
+
+        }
+
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+export async function followUser(user_id) {
+    try {
+        const response = await fetch(`${backend_url}/users/${user_id}/follow`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            credentials: "include",
+        });
+        if (response.ok) {
+            const data = await response.json();
+            console.log("User Following:", data);
+            return data;
+        } else {
+            throw new Error(response.statusText || "Unknown error");
+
+        }
+
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+export async function unfollowUser(user_id) {
+    try {
+        const response = await fetch(`${backend_url}/users/${user_id}/unfollow`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            credentials: "include",
+        });
+        if (response.ok) {
+            const data = await response.json();
+            console.log("User unfollowing:", data);
+            return data;
+        } else {
+            throw new Error(response.statusText || "Unknown error");
+
+        }
+
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+export async function getCurrentUser(token) {
+    try {
+        const response = await fetch(`${backend_url}/users/me`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+            credentials: "include"
+        });
+        if (response.ok) {
+            const data = await response.json();
+            console.log("Current User:", data);
+            return data;
+        } else {
+            throw new Error(response.statusText || "Unknown error");
+
+        }
+
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+export async function upgradeCurrentUser(token) {
+    try {
+        const response = await fetch(`${backend_url}/users/me`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+
+            },
+            credentials: "include"
+        });
+        if (response.ok) {
+            const data = await response.json();
+            console.log("Upgraded user data:", data);
+            return data;
+        }
+        else {
+            throw new Error(response.statusText || "Unknown error");
         }
     } catch (error) {
-        console.error("Error happened fetching users :", error);
-        return [];
+        console.log("Error:", error)
     }
 
 }
 
-export async function fetchSingleUser() {
+export const loginUser = async (userData) => {
     try {
-        const response = await fetch(config.backend_url + "/users/me", {
-            method: "GET",
-            credentials: "include",
+        const response = await fetch(`${backend_url}/auth/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(userData)
         });
-
-        if (response.ok) {
-            const data = await response.json();
-            return data;
-        }
-        else {
-            console.log("User couldn't be fetched");
-            return null;
+        if (!response.ok) {
+            throw new Error("Login failed");
         }
 
+        const token = response.headers.get("Authorization");
+        //! HEADERDAN TOKEN ALMA  
+
+        if (token) {
+            console.log("Token retrivied", token)
+            return token;
+        } else {
+            throw new Error("Token not found in headers");
+        }
 
     } catch (error) {
-        console.error("Error happened fetching single user :", error)
-
+        console.error(error);
+        toast.error(error.message.tr);
     }
 }
