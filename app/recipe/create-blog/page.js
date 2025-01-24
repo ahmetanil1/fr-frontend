98631c3f8f41db922f4b62f52cd123ebbc2b2c16
ahmetanil1 <amtyrgnc@gmail.com>
2025-01-24 16:59:12 +0300

    Add new blog states but didn't complete

--- app/recipe/create-blog/page.js
+++ app/recipe/create-blog/page.js
@@ -1,109 +1,35 @@
 "use client"
 import React, { useState } from 'react';
-import { toast } from 'react-toastify';
 import { FaCamera } from 'react-icons/fa';
 import { ImParagraphJustify } from "react-icons/im";
 import { IoMdVideocam } from "react-icons/io";
 import { FaTrash } from 'react-icons/fa';
-import { useRouter } from 'next/navigation';
-import dotenv from "dotenv"
 import Textarea from '@/components/general/Textarea';
 import Input from '@/components/general/Input';
 import Button from '@/components/general/Button';
+import useBlogFormDataStore from "@/store/useBlogFormDataStore";
+
+
 function CreateBlog() {
-    const [texts, setTexts] = useState([]);
-    const [images, setImages] = useState([]);
-    const [videos, setVideos] = useState([]);
-    const [text, setText] = useState('')
-    const [text2, setText2] = useState('')
-    const [imageUrls, setImageUrls] = useState([{ imageUrl: '', imageName: '' }])
-    const [imageName, setImageName] = useState('')
-    const router = useRouter();
-    const backend_url = process.env.BACKEND_URL;
-    const handleSubmitBlog = async (e) => {
-        e.preventDefault();
-        const data = { texts, images, videos };
-        console.log(data);
+    const {
+        blogFormData,
+        setBlogFormData,
+        updateField,
+        setField,
+        addContent,
+        removeContent,
+        updateContent,
+        addGallery,
+        removeGallery,
+        updateGallery,
+        addVideo,
+        removeVideo,
+        updateVideo
+    } = useBlogFormDataStore();
 
-        try {
-            const response = await fetch(`${backend_url}/blog`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(data)
-            });
-
-            if (response.ok) {
-                toast.success('Blog başarıyla oluşturuldu');
-            } else {
-                toast.error('Blog oluşturulurken bir hata oluştu');
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    const handleAddText = () => {
-        setTexts([...texts, '']);
-    };
-
-    const handleTextChange = (value, index) => {
-        const updatedTexts = [...texts];
-        updatedTexts[index] = value;
-        setTexts(updatedTexts);
-    };
-
-    const handleRemoveText = (index) => {
-        const updatedTexts = texts.filter((_, i) => i !== index);
-        setTexts(updatedTexts);
-    };
-
-    const handleAddAnotherImage = () => {
-        setImages([...images, { imageUrl: '', imageName: '' }]); // Yeni resim ekleme
-    };
-
-    const handleImageChange = (file, index) => {
-        if (file) {
-            const updatedImages = [...images];
-            updatedImages[index] = { imageUrl: URL.createObjectURL(file), imageName: file.name };
-            setImages(updatedImages);  // yalnızca ilgili index'i güncelle
-        }
-    };
-
-    const handleAddImage = (e, index) => {
-        const file = e.target.files[0];
-        if (file) {
-            const updatedImageUrls = [...imageUrls];
-            updatedImageUrls[index] = { imageUrl: URL.createObjectURL(file) }; // Update with new image URL
-            setImageUrls(updatedImageUrls);
-            setImageName(file.name); // Set the image file name
-        }
-    };
-
-    const handleRemoveImage = (index) => {
-        const updatedImages = images.filter((_, i) => i !== index);
-        setImages(updatedImages);
-
-    };
-
-    const handleAddVideo = () => {
-        setVideos([...videos, '']);
-    };
-
-    const handleVideoChange = (value, index) => {
-        const updatedVideos = [...videos];
-        updatedVideos[index] = value;
-        setVideos(updatedVideos);
-    };
-
-    const handleRemoveVideo = (index) => {
-        const updatedVideos = videos.filter((_, i) => i !== index);
-        setVideos(updatedVideos);
-    };
-
-    const handleTextChangeNormal = (e) => { setText(e.target.value) }
-    const handleTextChangeNormal2 = (e) => { setText2(e.target.value) }
+    const handleSubmitBlog = async () => {
+        // Call the API to create a new blog
+    }
 
     return (
         <div className="flex flex-col justify-center items-center">
@@ -118,8 +44,8 @@ function CreateBlog() {
                         <Textarea
                             size="sm"
                             placeholder="Write the title of your recipe here."
-                            value={text}
-                            onChange={handleTextChangeNormal}
+                            value={blogFormData.title}
+                            onChange={(e) => setField('title', e.target.value)}
                             maxLength="90"
                         />
 
@@ -129,8 +55,8 @@ function CreateBlog() {
                         <Textarea
                             size="md"
                             placeholder="Write the introductory text here where you talk about how delicious your recipe is."
-                            value={text2}
-                            onChange={handleTextChangeNormal2}
+                            value={blogFormData.description}
+                            onChange={(e) => setField('description', e.target.value)}
                             maxLength="1000"
                         />
                     </div>
@@ -140,23 +66,18 @@ function CreateBlog() {
                             htmlFor="photo-upload"
                             className="flex flex-col items-center justify-center cursor-pointer"
                         >
-                            {imageUrls[0] && imageUrls[0].imageUrl ? (
+                            {blogFormData.thumbnail ? (
                                 <div className="w-full h-auto flex flex-col items-center justify-center">
                                     <img
-                                        src={imageUrls[0].imageUrl}
+                                        src={blogFormData.thumbail.name || "thumbnail"}
                                         alt="Uploaded"
                                         className="w-48 h-48 object-cover rounded-md"
                                     />
-                                    <p className="text-gray-700 dark:text-gray-100 font-medium mt-2">
-                                        {imageName} {/* Display the image file name */}
-                                    </p>
                                     <button
                                         type="button"
                                         className="absolute top-3 right-2 "
                                         onClick={() => {
-                                            const newImageUrls = [...imageUrls];
-                                            newImageUrls[0] = { imageUrl: '', name: '' }; // Reset the image URL and name
-                                            setImageUrls(newImageUrls);
+                                            setBlogFormData({ ...blogFormData, thumbnail: '' });
                                         }}
                                     >
                                         <FaTrash />
@@ -181,31 +102,31 @@ function CreateBlog() {
                             type="file"
                             accept="image/*"
                             className="sr-only"
-                            onChange={(e) => handleAddImage(e, 0)}
+                            onChange={(e) => setField('thumbnail', URL.createObjectURL(e.target.files[0]))}
                         />
                     </div>
 
-                    {texts.slice(1).map((text, index) => (
+                    {blogFormData.content.slice(1).map((text, index) => (
                         <div key={`text-${index}`} className="relative items-center">
                             <Textarea
                                 size="md"
                                 placeholder="Write the introductory text here where you talk about how delicious your recipe is."
                                 value={text}
-                                onChange={(e) => handleTextChange(e.target.value, index + 1)}
+                                onChange={(e) => updateContent(e.target.value, index)}
                                 maxLength="1000"
                             />
-                            {texts.length > 0 && (
+                            {blogFormData.content.length > 0 && (
                                 <button
                                     type="button"
                                     className="absolute right-2 top-3 "
-                                    onClick={() => handleRemoveText(index + 1)}
+                                    onClick={() => removeContent(blogFormData.content[index])}
                                 >
                                     <FaTrash />
                                 </button>
                             )}
                         </div>
                     ))}
-                    <button type="button" className="flex items-center gap-2" onClick={handleAddText}>
+                    <button type="button" className="flex items-center gap-2" onClick={addContent}>
                         <div>
                             <ImParagraphJustify size={24} />
                         </div>
@@ -214,69 +135,61 @@ function CreateBlog() {
                         </div>
                     </button>
 
-                    {images.length > 0 && (
+                    {blogFormData.gallery.length > 0 && (
                         <div className='flex flex-wrap gap-6 '>
-                        {images.map((image, index) => (
-                            <div key={`image-${index}`}>
-                                <label htmlFor={`photo-upload-${index}`} className="relative cursor-pointer w-32 h-32 rounded-md flex flex-col items-center justify-center">
-                                    {image.imageUrl ? (
-                                        <div className="w-full h-auto flex flex-col items-center justify-center ">
-                                            <img
-                                                src={image.imageUrl}
-                                                alt="Uploaded"
-                                                className="w-32 h-32 object-cover rounded-md"
-                                            />
-                                            <p className="text-gray-700 dark:text-gray-100 font-medium mt-2">
-                                                {imageName}
-                                                {/*DİĞER İMAGE İLE AYNI İMAGE YAZILIYO FİXLE */}
-                                            </p>
+                            {blogFormData.gallery.map((image, index) => (
+                                <div key={`image-${index}`}>
+                                    <label htmlFor={`photo-upload-${index}`} className="relative cursor-pointer w-32 h-32 rounded-md flex flex-col items-center justify-center">
+                                        {image.imageUrl ? (
+                                            <div className="w-full h-auto flex flex-col items-center justify-center ">
+                                                <img
+                                                    src={image.imageUrl}
+                                                    alt="Uploaded"
+                                                    className="w-32 h-32 object-cover rounded-md"
+                                                />
+                                                <button
+                                                    type="button"
+                                                    className="absolute top-3 right-2 "
+                                                    onClick={() => removeGallery(blogFormData.gallery[index])}
+                                                >
+                                                    <FaTrash />
+                                                </button>
+                                            </div>
+                                        ) : (
+                                            <>
+                                                <div className="bg-gray-800 dark:bg-gray-100 w-20 h-20 rounded-full flex items-center justify-center">
+                                                    <FaCamera className="text-white dark:text-gray-800" size={36} />
+                                                </div>
+                                                <div className="mt-2">
+                                                    <span className="text-md text-gray-800 dark:text-gray-100">
+                                                        Upload Image
+                                                    </span>
+                                                </div>
+                                            </>
+                                        )}
+                                        {blogFormData.gallery.length > 0 && (
                                             <button
                                                 type="button"
                                                 className="absolute top-3 right-2 "
-                                                onClick={() => {
-                                                    const newImageUrls = [...imageUrls];
-                                                    newImageUrls[0] = { imageUrl: '', name: '' }; // Reset the image URL and name
-                                                    setImageUrls(newImageUrls);
-                                                }}
+                                                onClick={() => removeGallery(blogFormData.gallery[index])}
                                             >
                                                 <FaTrash />
                                             </button>
-                                        </div>
-                                    ) : (
-                                        <>
-                                            <div className="bg-gray-800 dark:bg-gray-100 w-20 h-20 rounded-full flex items-center justify-center">
-                                                <FaCamera className="text-white dark:text-gray-800" size={36} />
-                                            </div>
-                                            <div className="mt-2">
-                                                <span className="text-md text-gray-800 dark:text-gray-100">
-                                                    Upload Image
-                                                </span>
-                                            </div>
-                                        </>
-                                    )}
-                                    {images.length > 0 && (
-                                        <button
-                                            type="button"
-                                            className="absolute top-3 right-2 "
-                                            onClick={() => handleRemoveImage(index)}
-                                        >
-                                            <FaTrash />
-                                        </button>
-                                    )}
-                                    <input
-                                        id={`photo-upload-${index}`}
-                                        type="file"
-                                        accept="image/*"
-                                        className="sr-only"
-                                        onChange={(e) => handleImageChange(e.target.files[0], index)} // yalnızca ilgili index'i güncelle
-                                    />
-                                </label>
-                            </div>
-                        ))}
-                    </div>
-                    ) }
+                                        )}
+                                        <input
+                                            id={`photo-upload-${index}`}
+                                            type="file"
+                                            accept="image/*"
+                                            className="sr-only"
+                                            onchange={(e) => updateGallery(index, URL.createObjectURL(e.target.files[0]))}
+                                        />
+                                    </label>
+                                </div>
+                            ))}
+                        </div>
+                    )}
 
-                    <button type="button" className="flex items-center gap-2" onClick={handleAddAnotherImage}>
+                    <button type="button" className="flex items-center gap-2" onClick={addGallery}>
                         <div>
                             <FaCamera size={24} />
                         </div>
@@ -285,19 +198,19 @@ function CreateBlog() {
                         </div>
                     </button>
 
-                    {videos.map((video, index) => (
+                    {blogFormData.video.map((video, index) => (
                         <div key={`video-${index}`} className="relative items-center">
                             <Input
                                 size="wfull"
                                 placeholder={`Video ${index + 1}`}
                                 value={video}
-                                onChange={(e) => handleVideoChange(e.target.value, index)}
+                                onChange={(e) => updateVideo(e.target.value, index)}
                             />
-                            {videos.length > 0 && (
+                            {blogFormData.video.length > 0 && (
                                 <button
                                     type="button"
                                     className="absolute top-3 right-2 "
-                                    onClick={() => handleRemoveVideo(index)}
+                                    onClick={() => removeVideo(blogFormData.video[index])}
                                 >
                                     <FaTrash />
                                 </button>
@@ -305,7 +218,7 @@ function CreateBlog() {
                         </div>
                     ))}
 
-                    <button type="button" className="flex items-center gap-2" onClick={handleAddVideo}>
+                    <button type="button" className="flex items-center gap-2" onClick={addVideo}>
                         <div>
                             <IoMdVideocam size={24} />
                         </div>
