04416af1e3858b796ff7ca8c41ce7387ed4571b9
ahmetanil1 <amtyrgnc@gmail.com>
2025-01-28 18:30:34 +0300

    Changed name

---
+++ app/recipe/[[...id]]/page.js
@@ -0,0 +1,12 @@
+import RecipeDetail from '@/components/recipe-details'
+import React from 'react'
+
+function RecipeDetails() {
+    return (
+        <div>
+            <RecipeDetail />
+        </div>
+    )
+}
+
+export default RecipeDetails
