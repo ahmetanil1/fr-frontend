495979182b8a59c0bae494451a43f52b5e26976c
ahmetanil1 <amtyrgnc@gmail.com>
2025-02-03 20:43:02 +0300

    Changed content of userstore

--- store/useUserStore.js
+++ store/useUserStore.js
@@ -1,227 +1,91 @@
-import { create } from "zustand"
+import { create } from "zustand";
 
 const useUserStore = create((set) => ({
-    userData: {
-        fullName: [
-            {
-                name: "",
-                surname: "",
-            }
-        ],
-        setFullName: (fullName) => set(state => ({ userData: { ...state.userData, fullName } })),
-        username: "",
-        setUsername: (username) => set(state => ({ userData: { ...state.userData, username } })),
-        dob: [
-            {
-                day: "",
-                month: "",
-                year: "",
-            }
-        ],
-        setDob: (dob) => set(state => ({ userData: { ...state.userData, dob } })),
+    formData: {
+        fullName: {
+            name: "",
+            surname: ""
+        },
+        userName: "",
         profileImage: "",
-        setProfileImage: (profileImage) => set(state => ({ userData: { ...state.userData, profileImage } })),
-        gender: [
-            {
-                male: false,
-                female: false,
-                specify: false
+        gender: {
+            male: {
+                name: "male"
+            },
+            female: {
+                name: "female"
+            },
+            specify: {
+                name: ""
             }
-        ],
-        setGender: (gender) => set(state => ({ userData: { ...state.userData, gender } })),
+        },
         phone: "",
-        setPhone: (phone) => set(state => ({ userData: { ...state.userData, phone } })),
         email: "",
-        setEmail: (email) => set(state => ({ userData: { ...state.userData, email } })),
         password: "",
-        setPassword: (password) => set(state => ({ userData: { ...state.userData, password } })),
         roles: [],
-        setRoles: (roles) => set(state => ({ userData: { ...state.userData, roles } })),
         perms: [],
-        setPerms: (perms) => set(state => ({ userData: { ...state.userData, perms } })),
-        isEmailVerified: false,
-        setIsEmailVerified: (isEmailVerified) => set(state => ({ userData: { ...state.userData, isEmailVerified } })),
         bio: "",
-        setBio: (bio) => set(state => ({ userData: { ...state.userData, bio } })),
-        links: [
-            {
-                name: "",
-                link: "",
-            }
-        ],
-        setLinks: (links) => set(state => ({ userData: { ...state.userData, links } })),
+        isEmailVerified: false,
+        links: {
+            twitter: {
+                name: "twitter",
+                url: ""
+            },
+            facebook: {
+                name: "facebook",
+                url: ""
+            },
+            instagram: {
+                name: "instagram",
+                url: ""
+            },
+            youtube: {
+                name: "youtube",
+                url: ""
+            },
+        },
         followers: [],
-        setFollowers: (followers) => set(state => ({ userData: { ...state.userData, followers } })),
-        followings: [],
-        setFollowings: (followings) => set(state => ({ userData: { ...state.userData, followings } })),
+        following: [],
         bookmarks: [],
-        setBookmarks: (bookmarks) => set(state => ({ userData: { ...state.userData, bookmarks } })),
-        rates: [
-            {
-                recipe_id: "",
-                rate: "",
+        rates: [],
+        dob: {
+            day: {
+                name: "day",
+                max: 31,
+                min: 1
+            },
+            month: {
+                name: "month",
+                max: 12,
+                min: 1
+            },
+            year: {
+                name: "year",
+                max: 3000,
+                min: 0
             }
-        ],
-        setRates: (rates) => set(state => ({ userData: { ...state.userData, rates } })),
+        }
+    },
+
+    updateFormData: {
+        updateFullName: (name, surname) => set((state) => ({ formData: { ...state.formData, fullName: { name, surname } } })),
+        updateUserName: (userName) => set((state) => ({ formData: { ...state.formData, userName } })),
+        updateProfileImage: (profileImage) => set((state) => ({ formData: { ...state.formData, profileImage } })),
+        updateGender: (male, female, specify) => set((state) => ({ formData: { ...state.formData, gender: { male, female, specify } } })),
+        updatePhone: (phone) => set((state) => ({ formData: { ...state.formData, phone } })),
+        updateEmail: (email) => set((state) => ({ formData: { ...state.formData, email } })),
+        updatePassword: (password) => set((state) => ({ formData: { ...state.formData, password } })),
+        updateRoles: (roles) => set((state) => ({ formData: { ...state.formData, roles } })),
+        updatePerms: (perms) => set((state) => ({ formData: { ...state.formData, perms } })),
+        updateBio: (bio) => set((state) => ({ formData: { ...state.formData, bio } })),
+        updateIsEmailVerified: (isEmailVerified) => set((state) => ({ formData: { ...state.formData, isEmailVerified } })),
+        updateLinks: (twitter, facebook, instagram, youtube) => set((state) => ({ formData: { ...state.formData, links: { twitter, facebook, instagram, youtube } } })),
+        updateFollowers: (followers) => set((state) => ({ formData: { ...state.formData, followers } })),
+        updateFollowing: (following) => set((state) => ({ formData: { ...state.formData, following } })),
+        updateBookmarks: (bookmarks) => set((state) => ({ formData: { ...state.formData, bookmarks } })),
+        updateRates: (rates) => set((state) => ({ formData: { ...state.formData, rates } })),
+        updateDob: (day, month, year) => set((state) => ({ formData: { ...state.formData, dob: { day, month, year } } }))
     }
-}))
+}));
 
-updateFullName: (fullname) => {
-    const [name, surname] = fullName.split(' ');
-    set((state) => ({
-        userData: {
-            ...state.userData,
-            fullName: { name, surname }
-        }
-    }))
-}
-
-updateUsername: (username) =>
-    set((state) => ({
-        userData: {
-            ...state.userData,
-            username
-        }
-    }))
-
-
-updateDob: (dob) => {
-    const [day, month, year] = dob.split('/');
-    set((state) => ({
-        userData: {
-            ...state.userData,
-            dob: { day, month, year }
-        }
-    }))
-}
-
-updateProfileImage: (profileImage) =>
-    set((state) => ({
-        userData: {
-            ...state.userData,
-            profileImage
-        }
-    }))
-
-updateGender: (genderType) =>
-    set((state) => ({
-        userData: {
-            ...state.userData,
-            gender: {
-                male: genderType === 'male',
-                female: genderType === 'female',
-                specify: genderType === 'specify'
-            }
-        }
-    }));
-
-updatePhone: (phone) =>
-    set((state) => ({
-        userData: {
-            ...state.userData,
-            phone
-        }
-    }))
-
-
-updateEmail: (email) =>
-    set((state) => ({
-        userData: {
-            ...state.userData,
-            email
-        }
-    }))
-
-
-updatePassword: (password) =>
-    set((state) => ({
-        userData: {
-            ...state.userData,
-            password
-        }
-    }))
-
-
-updateRoles: (roles) =>
-    set((state) => ({
-        userData: {
-            ...state.userData,
-            roles
-        }
-    }))
-
-
-updatePerms: (perms) =>
-    set((state) => ({
-        userData: {
-            ...state.userData,
-            perms
-        }
-    }))
-
-
-updateIsEmailVerified: (isEmailVerified) =>
-    set((state) => ({
-        userData: {
-            ...state.userData,
-            isEmailVerified
-        }
-    }))
-
-
-updateBio: (bio) =>
-    set((state) => ({
-        userData: {
-            ...state.userData,
-            bio
-        }
-    }))
-
-
-updateLinks: (links) => {
-    const { name, link } = links;
-    set((state) => ({
-        userData: {
-            ...state.userData,
-            links: { name, link }
-        }
-    }))
-
-}
-updateFollowers: (followers) =>
-    set((state) => ({
-        userData: {
-            ...state.userData,
-            followers
-        }
-    }))
-
-
-updateFollowings: (followings) =>
-    set((state) => ({
-        userData: {
-            ...state.userData,
-            followings
-        }
-    }))
-
-
-
-updateBookmarks: (bookmarks) =>
-    set((state) => ({
-        userData: {
-            ...state.userData,
-            bookmarks
-        }
-    }))
-
-
-updateRates: (rates) =>
-    set((state) => ({
-        userData: {
-            ...state.userData,
-            rates
-        }
-    }))
-
-export default useUserStore
+export default useUserStore;
