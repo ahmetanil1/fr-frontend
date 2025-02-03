0134764c834c5102e49e4efe5c6b538a9aceb604
ahmetanil1 <amtyrgnc@gmail.com>
2025-02-03 20:44:47 +0300

    Add new button and dynamic routes

--- components/header/profile.js
+++ components/header/profile.js
@@ -1,10 +1,34 @@
-import React, { useState } from "react";
+"use client"
+import React, { useState, useEffect } from "react";
 import { CgProfile } from "react-icons/cg";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
+import { getCurrentUser } from "@/services/users";
 
 function Profile({ user, onLogout }) {
+    const [user_id, setUser_id] = useState(user?.id || "");
+    const [userName, setUserName] = useState(user?.username || "");
     const [openMenu, setOpenMenu] = useState(false);
+    const router = useRouter();
+
+    useEffect(() => {
+        const fetchSingleUser = async () => {
+            try {
+                const response = await getCurrentUser();
+                if (response?.data) {
+                    setUser_id(response.data._id);
+                    setUserName(response.data.username);
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        if (!userName) {
+            fetchSingleUser();
+        }
+    }, [userName]);
+
     const handleLogout = async () => {
         const result = await onLogout();
         if (result) {
@@ -12,22 +36,20 @@ function Profile({ user, onLogout }) {
         } else {
             toast.error("Logout failed.");
         }
-    }
+    };
 
     return (
         <div className="relative inline-block text-left">
             <div onClick={() => setOpenMenu(!openMenu)} className="cursor-pointer">
                 {user ? (
-                    <>
-                        <img
-                            src={user.profileImage ||
-                                (user.gender === "male" ?
-                                    "https://via.placeholder.com/150?text=Default+Male+Avatar" :
-                                    "https://via.placeholder.com/150?text=Default+Female+Avatar")}
-                            alt={user.name}
-                            className="w-8 h-8 rounded-full"
-                        />
-                    </>
+                    <img
+                        src={user.profileImage ||
+                            (user.gender === "male"
+                                ? "https://via.placeholder.com/150?text=Default+Male+Avatar"
+                                : "https://via.placeholder.com/150?text=Default+Female+Avatar")}
+                        alt={user.name}
+                        className="w-8 h-8 rounded-full"
+                    />
                 ) : (
                     <CgProfile size="35" />
                 )}
@@ -39,15 +61,23 @@ function Profile({ user, onLogout }) {
                         {user ? (
                             <>
                                 <Link
-                                    href="/user"
+                                    href={`/profile/${userName}`}
+                                    className="px-4 py-2 text-gray-700 hover:bg-gray-200 cursor-pointer"
+                                >
+                                    Profile
+                                </Link>
+                                <Link
+                                    href={`/user/${user_id}`}
                                     className="px-4 py-2 text-gray-700 hover:bg-gray-200 cursor-pointer"
                                 >
                                     Recipes
                                 </Link>
                                 <Link
-                                    href="/settings"
+                                    href={`/settings/${userName}`}
                                     className="px-4 py-2 text-gray-700 hover:bg-gray-200 cursor-pointer"
-                                >Settings</Link>
+                                >
+                                    Settings
+                                </Link>
                                 <Link
                                     href="/login"
                                     onClick={handleLogout}
