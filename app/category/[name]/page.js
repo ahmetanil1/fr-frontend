abf36a8132f0a5c0d2ca67577c579fa4df7e9e2f
ahmetanil1 <amtyrgnc@gmail.com>
2025-02-03 20:41:48 +0300

    Add new category details page

---
+++ app/category/[name]/page.js
@@ -0,0 +1,15 @@
+import CategoryContainer from '@/containers/category'
+import React from 'react'
+
+async function CategoryPage() {
+    return (
+        <div>
+            <div>
+                asda
+            </div>
+        </div>
+    )
+
+}
+
+export default CategoryPage
