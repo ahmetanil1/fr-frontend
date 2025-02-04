a3004797904e6d6d5741a7c6bac9a53267c42788
ahmetanil1 <amtyrgnc@gmail.com>
2025-02-04 19:33:16 +0300

    Fetched data to home page and designed

--- containers/home/index.js
+++ containers/home/index.js
@@ -1,39 +1,27 @@
 import React from "react";
-import CategoryContainer from "../category";
-
+import MostPopuler from "../../components/HomeComponents/MostPopular";
+import MostRating from "../../components/HomeComponents/MostRating";
+import Recommended from "../../components/HomeComponents/Recommended";
+import AllRecipes from "../../components/HomeComponents/AllRecipes";
 
 const HomeContainer = () => {
 
     return (
-        <div>
-            {/*General Div*/}
-            <div>
-                <div>
-                    <div>
-                        <CategoryContainer />
-                    </div>
-                </div>
-                <div>
-                    <div>
-                        {/*  Most Rating Recipe */}
-                    </div>
-                </div>
-                <div>
-                    <div>
-                        {/*  Most Popular Recipes */}
-                    </div>
-                </div>
-                <div>
-                    <div>
-                        {/* With videos */}
-                    </div>
-                </div>
-                <div>
-                    <div>
-                        {/* See More */}
-                    </div>
-                </div>
-            </div>
+        <div className="flex flex-col space-y-10 px-4 py-10">
+            <span className="flex justify-center items-center text-2xl ">Most Popular</span>
+
+            <div><MostPopuler /></div>
+            <hr className="border border-gray-400"></hr>
+            <span className="flex justify-center items-center text-2xl ">Most Rating</span>
+            <div><MostRating /></div>
+            <hr className="border border-gray-400"></hr>
+            <span className="flex justify-center items-center text-2xl ">Recommended</span>
+
+            <div><Recommended /></div>
+            <hr className="border border-gray-400"></hr>
+            <span className="flex justify-center items-center text-2xl ">All Recipes</span>
+
+            <div><AllRecipes /></div>
         </div>
     )
 }
